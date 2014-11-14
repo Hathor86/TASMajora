8a50144f587f7f75495cd1c0a5b184e06d14280e
Hathor86 <francois.guiot@gmail.com>
2014-11-14 22:23:27 +0100

    Work of 14/11/2014
    Some work on UI

--- Scripts/Legend of Zelda, The - Majora's Mask (USA).lua
+++ Scripts/Legend of Zelda, The - Majora's Mask (USA).lua
@@ -1,10 +1,13 @@
 local skutullaCode1stByte = 0x1F0665;
 local bombersCode1stByte = 0x1F066B;
+local lotteryCode1stByte = 0x1F065F;
 
 local xPosition = 0x3FFDD4;
 local yPosition = 0x3FFDD8;
 local zPosition = 0x3FFDDC;
+local internalSpeed = 0x3FFE20;
 
+local currentDay = 0x1EF68B;
 local igTime = 0x1EF67C;
 
 local oneSecond = 86400 / 65535; --Number of real seconds / IG seconds. FF FF (65535) is midnight
@@ -13,6 +16,7 @@ local spacing = 12;
 
 local bombersCode = "";
 local skulltullaCode = {};
+local lotteryCodes = {};
 
 --Init stuff. this can be called once
 local function init()
@@ -23,6 +27,16 @@ local function init()
 	bombersCode = "";
 	for i = 0, 4 do
 		bombersCode = string.format("%s%s", bombersCode, memory.readbyte(bombersCode1stByte + i));
+	end
+	
+	local k = 0;
+	for i = 0, 2 do
+		lotteryCodes[i] = {};
+		for j = 0, 2 do
+			lotteryCodes[i][j] = memory.readbyte(lotteryCode1stByte + k);
+			k = k + 1;
+		end
+		k = k + 1;
 	end
 end
 
@@ -61,6 +75,10 @@ local function codes()
 		gui.text(250 + i * 20, firstLine + spacing, string.format("%s%i", tab, skulltullaCode[i]), _, color);
 	end
 	gui.text(0, firstLine + spacing, string.format("Skulltula Code (%s jumps):", jumpCount));
+	local currentDayValue = memory.readbyte(currentDay);
+	if currentDayValue == 1 or currentDayValue == 2 or currentDayValue == 3 then
+		gui.text(0, firstLine + spacing * 2, string.format("Lottery code: %i%i%i", lotteryCodes[currentDayValue - 1][0], lotteryCodes[currentDayValue - 1][1], lotteryCodes[currentDayValue - 1][2]));
+	end
 end
 
 --Display coords
@@ -70,11 +88,15 @@ local function coord()
 	gui.text(0, firstLine + spacing * 2, string.format("Z-position: %.3f", memory.readfloat(zPosition, true)), _, _, "topright");
 end
 
+local function speed()
+	gui.text(0, firstLine + spacing * 3, string.format("Internal speed: %.3f", memory.readfloat(internalSpeed, true)), _, _, "topright");
+end
+
 --Display accurate IG clock
 local function clockDisplay()
 	local hour = math.floor(memory.read_u16_be(igTime) * oneSecond / 3600);
 	local decimalMinutes = (memory.read_u16_be(igTime) * oneSecond / 3600) - hour;
-	gui.text(300, 3, string.format("%.0f:%.0f", hour, decimalMinutes * 60), _, _, "bottomleft");
+	gui.text(250, 3, string.format("%02.0f:%02.0f:%02.0f (day %i)", hour, math.floor(decimalMinutes * 60), (decimalMinutes * 60 - math.floor(decimalMinutes * 60)) * 60, memory.readbyte(currentDay)), _, _, "bottomleft");
 end
 
 --General UI call
@@ -82,6 +104,7 @@ local function ui()
 	--init();
 	codes();
 	coord();
+	speed();
 	clockDisplay();
 end
 
