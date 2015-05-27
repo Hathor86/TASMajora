d34d81fe5109871fb622b60f56909d3e9852ee18
Hathor86 <francois.guiot@gmail.com>
2015-05-27 22:46:32 +0200

    Nearly bow
    Fight against mini-boss can be improved. I have to mind it.

--- Scripts/MM_Form_AdvancedControl.lua
+++ Scripts/MM_Form_AdvancedControl.lua
@@ -3,6 +3,7 @@ AdditionalWindows.AdvancedConrols = {};
 local handles = {};
 local flags = {};
 flags["song"] = 0;
+flags["revertcam"] = 0;
 
 local function GotoExit()
 	memory.write_s16_be(MM.Watch.Misc.ExitSetter, MM.Dictionnary.ExitsByName[forms.getproperty(handles["exitDropDown"], "Text")]);
@@ -18,11 +19,13 @@ AdditionalWindows.AdvancedConrols.Init = function()
 
 	forms.label(form, "Song Player", 0, 0, 75, 15, false);
 	handles["song"] = forms.dropdown(form, {"SOT" ,"SODT", "SOH", "SOE", "SOS", "SOST", "SOA", "GLI", "GL", "NWBN", "EOE", "OOA"}, 0, 15, 50, 15);
-	forms.button(form, "Play", function() SetFlag("song") end, 55, 15, 35, 20);
+	forms.button(form, "Play", function() SetFlag("song") end, 55, 15, 35, 22);
 	
 	forms.label(form, "Exit Setter", 0, 45, 75, 15, false);
 	handles["exitDropDown"] = forms.dropdown(form, MM.Dictionnary.Exits, 0, 60, 125, 15);
-	handles["goToExitButton"] = forms.button(form, "Go", GotoExit, 130, 60, 35, 20);
+	handles["goToExitButton"] = forms.button(form, "Go", GotoExit, 130, 60, 35, 22);
+	
+	forms.button(form, "Revert Camera Angle", function() SetFlag("revertcam") end, 0, 85, 125, 22);
 end
 
 AdditionalWindows.AdvancedConrols.Refresh = function()
@@ -36,5 +39,10 @@ AdditionalWindows.AdvancedConrols.Refresh = function()
 	if (flags["song"] == 1) then
 		flags["song"] = 0;
 		MovementsHelper.PlaySong(forms.getproperty(handles["song"], "Text"));
+	end
+	
+	if (flags["revertcam"] == 1) then
+		flags["revertcam"] = 0;
+		MovementsHelper.ReverseCameraAngle(0);
 	end
 end
